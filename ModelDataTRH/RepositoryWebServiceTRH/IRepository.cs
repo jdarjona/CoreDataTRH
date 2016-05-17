c193be9b1bf25077567488f0e58c3da793083da9
JuandeDios <JuandeDios@LIEGE-PC>
2016-05-17 10:27:38 +0200

    Creacion Proyectos repositorios

---
+++ ModelDataTRH/RepositoryWebServiceTRH/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace RepositoryWebServiceTRH
+{
+    public interface IRepository<TEntity,Tid> where TEntity:class
+    {
+
+        TEntity Get(Tid id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entitties);
+
+        void Reove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        
+
+    }
+}
