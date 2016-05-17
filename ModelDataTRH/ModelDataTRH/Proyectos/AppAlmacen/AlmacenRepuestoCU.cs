c53ef02ee718528962654797e90bc596dd7a9e19
f.plata <f.plata@trh-es.com>
2016-05-17 09:45:59 +0200

    Clase Item terminada

--- ModelDataTRH/ModelDataTRH/Proyectos/AppAlmacen/AlmacenRepuestoCU.cs
+++ ModelDataTRH/ModelDataTRH/Proyectos/AppAlmacen/AlmacenRepuestoCU.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModelDataTRH.Proyectos.AppAlmacen
+namespace ModelDataTRH.Proyectos
 {
     class AlmacenRepuestoCU
     {
+        
     }
 }
--- ModelDataTRH/ModelDataTRH/Proyectos/AppAlmacen/EntragaDeEpis.cs
+++ ModelDataTRH/ModelDataTRH/Proyectos/AppAlmacen/EntragaDeEpis.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModelDataTRH.Proyectos.AppAlmacen
+namespace ModelDataTRH.Proyectos
 {
     class EntragaDeEpis
     {
